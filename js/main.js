a91e2d7a14891ebc321f6ffc88259694f29fae4e
w4721x <w4721x@w4721x.local>
2018-09-06 13:13:16 +0530

    Added Form Validations

--- js/main.js
+++ js/main.js
@@ -3,9 +3,14 @@ document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
 // save Bookmark
 function saveBookmark(e) {
-    // GEt Form Values
+    // Get Form Values
     var siteName = document.getElementById('siteName').value;
     var siteUrl = document.getElementById('siteUrl').value;
+
+    if(!validateForm(siteName, siteUrl)){
+        return false;
+    }
+
     var bookmark = {
         name: siteName,
         url: siteUrl
@@ -27,6 +32,9 @@ function saveBookmark(e) {
         //Re-set back to Local Storage
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks)); 
     }
+    //clear form after successful submit
+    document.getElementById('myForm').reset();
+    
     //Re-fetch bookmarks
     fetchBookmarks();
 
@@ -43,7 +51,8 @@ function deleteBookmark(url) {
         if(bookmarks[i].url == url){
             bookmarks.splice(i, 1);
         }
-    }
+    }    
+    
     //Re-set back to Local Storage
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks)); 
 
@@ -77,4 +86,21 @@ function fetchBookmarks() {
                                       '</h3>'
                                       '</div>';
     }
+}
+
+// Validate Form 
+function validateForm(siteName, siteUrl) {
+    if(!siteName || !siteUrl) {
+        alert('Site Name & URL both required');
+        return false;
+    }
+    
+    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+    if(!siteUrl.match(regex)){
+        alert('Please Enter valid URL');
+        return false;
+    }
+
+    return true;
 }
